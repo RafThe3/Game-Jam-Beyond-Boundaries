26be2b241e46d00f623523861a64a9f385d571da
Raf <25.rafael.gutierrez@ksd.org>
2023-12-07 12:17:09 -0800

    changes to player script

--- Beyond Boundaries 2/Assets/Player.cs
+++ Beyond Boundaries 2/Assets/Player.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
 
-public class TopDownMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    #region General
+    //Internal Variables
     private Rigidbody2D rb;
+    #endregion
+
+    #region Movement
+    [Header("Movement")]
     [Min(1), SerializeField] private float moveSpeed = 5.0f;
+    #endregion
 
     private void Start()
     {
@@ -15,9 +22,10 @@ public class TopDownMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        #region Movement
         float x = Input.GetAxisRaw("Horizontal"), y = Input.GetAxisRaw("Vertical");
 
         rb.velocity = 100 * moveSpeed * Time.deltaTime * new Vector2(x, y);
-
+        #endregion
     }
 }
