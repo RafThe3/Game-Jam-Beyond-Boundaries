31e91b09cd964c51b8d778525e2715cfc3d62a9d
Seraph-21 <147756663+Seraph-21@users.noreply.github.com>
2023-12-12 12:23:00 -0800

    adding X amount of speed to player each exp

--- Beyond Boundaries 2/Assets/Scripts/Player Lvl Stuff/LvlProgress.cs
+++ Beyond Boundaries 2/Assets/Scripts/Player Lvl Stuff/LvlProgress.cs
@@ -7,17 +7,32 @@ public class LvlProgress : MonoBehaviour
 {
     [SerializeField] int levelProgress = 0;
     [SerializeField] TextMeshProUGUI myText;
+    [SerializeField] float expBoost;
+    private bool levelIncreased;
+    public float x;
+    [SerializeField] float i;
 
-    void Update()
+    private void Start()
     {
-        
+        levelIncreased = false;
     }
+
     void OnTriggerEnter2D(Collider2D LvlCollided)
     {
         if (LvlCollided.gameObject.tag == "LvlDrops")
         {
+            i++;
             levelProgress++;
-            myText.text = "Level:" + levelProgress;
+            myText.text = "Exp:" + levelProgress + "/10";
+            levelIncreased = true;
+            if (levelIncreased == true)
+            {
+                Debug.Log(message: x = (float)(5 + 0.20 * i));
+            }
+        }
+        else
+        {
+            levelIncreased = false;
         }
     }
 }
