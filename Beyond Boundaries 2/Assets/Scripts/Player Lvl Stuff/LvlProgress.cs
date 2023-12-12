e7f56048ca84bb7f6afb6977c541d960b3d9d4ea
Seraph-21 <147756663+Seraph-21@users.noreply.github.com>
2023-12-12 12:46:52 -0800

    removed unnessary lines

--- Beyond Boundaries 2/Assets/Scripts/Player Lvl Stuff/LvlProgress.cs
+++ Beyond Boundaries 2/Assets/Scripts/Player Lvl Stuff/LvlProgress.cs
@@ -8,13 +8,15 @@ public class LvlProgress : MonoBehaviour
     [SerializeField] int levelProgress = 0;
     [SerializeField] TextMeshProUGUI myText;
     [SerializeField] float expBoost;
-    private bool levelIncreased;
     public float x;
-    [SerializeField] float i;
+    private float y;
+    [SerializeField] float i; 
+    
 
-    private void Start()
+    void Start()
     {
-        levelIncreased = false;
+        //y = Player.moveSpeed;
+
     }
 
     void OnTriggerEnter2D(Collider2D LvlCollided)
@@ -24,15 +26,10 @@ public class LvlProgress : MonoBehaviour
             i++;
             levelProgress++;
             myText.text = "Exp:" + levelProgress + "/10";
-            levelIncreased = true;
-            if (levelIncreased == true)
-            {
-                Debug.Log(message: x = (float)(5 + 0.20 * i));
-            }
+
+            Debug.Log(message: x = (float)(y + 0.20 * i));
+            Debug.Log(message: x = (float)(5 + 0.20 * i));
         }
-        else
-        {
-            levelIncreased = false;
-        }
+       
     }
 }
